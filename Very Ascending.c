977a0a14119a3ef9e1edea505a6443f39014f77e
Kashish811 <kashish081106@gmail.com>
2025-04-08 19:53:01 +0530

    Very Ascending.c

--- Very Ascending.c
+++ Very Ascending.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
-int main()
-{
-	int size, i, previousValue, currentValue;
-	int veryAscendingFlag=1;
-	
-	printf("Enter the size:");
-	scanf("%d", &size);
-	
-	if(size<0){
-		printf("Enter a positive size.");
-	}
-	else{
-		do{
-			printf("Enter a value:");
-			scanf("%d", &currentValue);
-			
-			if(currentValue<= previousValue){
-				veryAscendingFlag=0;
-			}
-			previousValue = currentValue;
-			
-			size--;
-		}while(size>0);
-	}
-	if(veryAscendingFlag==1){
-		printf("Sequence is Very Ascending.\n");
-	} else{
-		printf("Sequence is not Very Ascending.\n");
-	}
-	return 0;
-	
+
+int main() {
+    int size, i, previousValue, currentValue;
+    int veryAscendingFlag = 1;
+
+    printf("Enter the size: ");
+    scanf("%d", &size);
+
+    if (size <= 0) {
+        printf("Enter a positive size.");
+    } else {
+        printf("Enter a value: ");
+        scanf("%d", &previousValue); // Take the first value outside the loop
+        size--; // Already read one number
+
+        while (size > 0) {
+            printf("Enter a value: ");
+            scanf("%d", &currentValue);
+
+            if (currentValue <= previousValue) {
+                veryAscendingFlag = 0;
+            }
+
+            previousValue = currentValue;
+            size--;
+        }
+    }
+
+    if (veryAscendingFlag == 1) {
+        printf("Sequence is Very Ascending.\n");
+    } else {
+        printf("Sequence is not Very Ascending.\n");
+    }
+
+    return 0;
 }
+
