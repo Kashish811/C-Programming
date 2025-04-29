7fa2301fc303bc39504dbbe2b015a670622a4f9c
Kashish811 <kashish081106@gmail.com>
2025-04-29 18:44:51 +0530

    🔢 Maximum Digit Finder (Two-Digit Number)
    This C program takes a two-digit number as input and returns the maximum digit among the two. It ensures basic input validation and demonstrates conditional logic and function usage.
    
    🧠 Concepts & Functions Used:
    #include <stdio.h> – for standard input/output operations 📥📤
    User-defined function: MaxDigit(int n) returns the larger of the two digits 🧮
    Modulus (%) and division (/) operators – to extract ones and tens digits 🔍
    Conditional Statements (if, else) – for decision-making based on digit comparison 🧭
    Basic input validation – checking if the number is two-digit before proceeding ✔️
    scanf and printf – to read user input and display output 🖨️

---
+++ Max Digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int MaxDigit(int n)
+{
+	int ones, tens;
+	ones = n%10;
+	tens = n/10;
+	if(ones>tens){
+		return ones;
+	}else{
+		return tens;
+	}		
+}
+	
+int main()
+{	
+	int num, result;
+	printf("Enter a two-digit number: ");
+	scanf("%d", &num);
+	
+	if(1<=(num%10)<10){
+	    result = MaxDigit(num);
+		printf("The maximum digit in the given number %d is %d", num,result);
+	}else{
+		printf("Enter a TWO-digit number only!");
+	}
+}
