51437e122a56b1f11a46998f745e4c2d1c327421
Kashish811 <kashish081106@gmail.com>
2025-06-09 18:26:55 +0530

    Factorial Calculation using Recursion
    This program calculates the factorial of a user-input number using a recursive function. It prompts the user for input, applies recursion to compute the factorial, and displays the result
    
    Functions & Concepts Used:
    factorial(int n) function for recursion 🌀
    Conditional statements (if-else) ⚖️
    printf and scanf for input/output 🖨️
    Recursive function calls 🔁
    Integer variables and return values 🔢

--- Factorial.c
+++ Factorial.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-
 int factorial(int n)
 {
 	if(n<1){
 		return 1;
 	}else{
 		return n*factorial(n-1);
-	}		
+	}
 }
-	
+
 int main()
-{	
-	int num, result;
-	printf("Enter a number: ");
-	scanf("%d", &num);
+{
+	int num, s;
+	printf("Please enter a number:");
+	scanf("%d",&num);
 	
-	result=factorial(num);
-	printf("Factorial of %d is %d!=%d",num,num,result);
+	s=factorial(num);
+	
+	printf("The factorial of %d is %d!=%d.",num, num, s);
+	
+	return 0;
 }
